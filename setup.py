ef084b187a43ced057fdd09b2deb7f041f4c73b2
NikhilVarshney92 <nikhilvarshney92@gmail.com>
2023-03-31 15:39:31 +0530

    Requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+# This setup.py is used to create ourwhole project as a package 
+
+from setuptools import find_packages,setup
+from typing import List
+
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)-> List[str]:
+    '''
+    This function will return the list of requirements.
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [r.replace("\n","") for r in requirements]
+    
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name = 'AUTOMATIC_MODEL_CREATOR',
+    version= '0.0.1',
+    author= 'Nihkil Varshney',
+    author_email='nikhilvarshney92@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
