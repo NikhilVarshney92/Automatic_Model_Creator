cc79514d61dc867142613c63914ef3873226af57
NikhilVarshney92 <nikhilvarshney92@gmail.com>
2023-04-01 12:45:55 +0530

    Data Transformer Created

---
+++ src/utils.py
@@ -0,0 +1,24 @@
+# pyright: reportMissingModuleSource=false
+# pyright: reportShadowedImports=false
+# pyright: reportMissingImports=false
+
+import os
+import sys
+import dill
+import pickle
+from logger import logging
+
+from src.exception import CustomException
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+            logging.info('Saved Successfully !!!')
+
+    except Exception as e:
+        raise CustomException(e, sys)
